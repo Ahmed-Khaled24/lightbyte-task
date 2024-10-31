f734b8bf20f56559b54c93e4b015a2513496c3db
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-10-31 13:13:29 +0300

    fix(schemas): pagination schema
    make it require string but the string must be
    a number so we can use it across the network to
    paginate the data

--- src/schemas/pagination.schema.ts
+++ src/schemas/pagination.schema.ts
@@ -1,6 +1,24 @@
 import { z } from "zod";
 
+/**
+ * Page is the offset from beginning @default 1
+ * Limit is the number of items in page @default 20
+ */
 export const PaginationZodSchema = z.object({
-    page: z.number().int().positive().optional(),
-    limit: z.number().int().positive().optional(),
+    page: z
+        .string()
+        .transform(Number)
+        .refine((number) => !isNaN(number), {
+            message: "Page must be a number",
+        })
+        .default("0"),
+    limit: z
+        .string()
+        .transform(Number)
+        .refine((number) => !isNaN(number), {
+            message: "limit must be a number",
+        })
+        .default("20"),
 });
+
+export type Pagination = z.infer<typeof PaginationZodSchema>;
