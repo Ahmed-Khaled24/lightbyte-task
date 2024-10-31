1c7195a47b0b247fe2a70035aad9d59a111b409e
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-10-31 11:05:57 +0300

    feat: create server initialization logic
    connect to database and start listening on the
    specified port

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
-console.log(process.env.MONGO_URL);
+import http from "node:http";
+import app from "./app";
+import mongoose from "mongoose";
+import { validateEnvVariables } from "./utils/env";
+
+async function initialization() {
+    const env = validateEnvVariables();
+
+    const server = http.createServer(app);
+
+    await mongoose.connect(env.MONGO_URL);
+
+    server.listen(process.env.PORT, () => {
+        console.log(`Server running on port ${process.env.PORT}`);
+    });
+}
+
+initialization();
