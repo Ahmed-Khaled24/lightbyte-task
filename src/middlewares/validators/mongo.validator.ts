8fcd36f514525e1f81c1710687271a0baaff4d21
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-10-31 13:15:29 +0300

    feat(middlewares): mongo validator middleware

---
+++ src/middlewares/validators/mongo.validator.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+import { z } from "zod";
+import mongoose from "mongoose";
+
+function IdParam(req: Request, res: Response, next: NextFunction) {
+    const IdSchema = z.object({
+        id: z.string().refine(
+            (id) => {
+                try {
+                    new mongoose.Types.ObjectId(id);
+                    return true;
+                } catch {
+                    return false;
+                }
+            },
+            {
+                message: "Invalid MongoDB Id",
+            },
+        ),
+    });
+
+    try {
+        IdSchema.parse(req.params);
+        next();
+    } catch (error) {
+        res.status(400).send({
+            data: `Invalid Id: ${(error as Error).message}`,
+        });
+    }
+}
+
+export default {
+    IdParam,
+};
