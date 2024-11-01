9a47e9e4330ddf3bf63bcfec437c880eb39ac0d2
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-11-01 08:42:08 +0200

    feat(services): update events service
    apply 5-per-day limit on creating events

--- src/services/events.service.ts
+++ src/services/events.service.ts
@@ -1,7 +1,30 @@
 import eventsRepository from "../repositories/events.repository";
 import { Event } from "../schemas/events.schema";
 
-async function createEvent(event: Event) {
+async function createEvent(event: Omit<Event, "id" | "attendees">) {
+    const creator = event.createdBy;
+
+    // Get event created today by the current creator
+    const creatorEvents = await eventsRepository.getEventsByUsername(creator);
+    const eventsCreatedToday = creatorEvents.filter((event) => {
+        const today = new Date();
+        const eventDate = new Date(event.date);
+
+        return (
+            today.getDate() === eventDate.getDate() &&
+            today.getFullYear() === eventDate.getFullYear() &&
+            today.getMonth() === eventDate.getMonth()
+        );
+    });
+
+    // Check the 5-per-day limit
+    if (eventsCreatedToday.length > 5) {
+        throw new HTTPError(
+            "Invalid operation: User can at most create 5 events per day, this is your sixths, come back tomorrow.",
+            403,
+        );
+    }
+
     return await eventsRepository.createEvent(event);
 }
 
