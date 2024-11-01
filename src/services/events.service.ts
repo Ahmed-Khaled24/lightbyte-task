d80f6e259085a997db36314006cac1eab8cd408b
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-11-01 09:15:41 +0200

    feat(services): finish up events service
    events service functionality is now complete

--- src/services/events.service.ts
+++ src/services/events.service.ts
@@ -1,5 +1,6 @@
 import eventsRepository from "../repositories/events.repository";
-import { Event } from "../schemas/events.schema";
+import { Event, UpdateEvent } from "../schemas/events.schema";
+import { HTTPError } from "../utils/HTTPError";
 
 async function createEvent(event: Omit<Event, "id" | "attendees">) {
     const creator = event.createdBy;
@@ -40,9 +41,77 @@ async function deleteEvent(id: string) {
     return await eventsRepository.deleteEvent(id);
 }
 
+async function updateEvent(id: string, event: UpdateEvent) {
+    return await eventsRepository.updateEvent(id, event);
+}
+
+async function subscribeEvent(id: string, username: string) {
+    const [targetEvent] = await eventsRepository.getEvents(id, 0, 0);
+
+    if (!targetEvent) {
+        throw new HTTPError("Event not found!", 404);
+    }
+
+    // Check if the event still in the future
+    const eventDate = new Date(targetEvent.date);
+    if (eventDate < new Date()) {
+        throw new HTTPError("Can not RSVP to ended event", 403);
+    }
+
+    // Check if user is already subscribed
+    const eventAttendees = targetEvent.attendees;
+    const alreadySubscribed = eventAttendees.find(
+        (attendee) => attendee === username,
+    );
+    if (alreadySubscribed) {
+        throw new HTTPError("Already RSVPed to this event", 409);
+    }
+
+    // Add the user to attendees list
+    const newAttendees: Event["attendees"] = [
+        ...targetEvent.attendees,
+        username,
+    ];
+
+    return await eventsRepository.updateEvent(id, { attendees: newAttendees });
+}
+
+async function unsubscribeEvent(id: string, username: string) {
+    const [targetEvent] = await eventsRepository.getEvents(id, 0, 0);
+
+    if (!targetEvent) {
+        throw new HTTPError("Event not found!", 404);
+    }
+
+    // Check if the event still in the future
+    const eventDate = new Date(targetEvent.date);
+    if (eventDate < new Date()) {
+        throw new HTTPError("Can not un-RSVP from ended event", 403);
+    }
+
+    // Check if user is already unsubscribed
+    const eventAttendees = targetEvent.attendees;
+    const attendeeIndex = eventAttendees.findIndex(
+        (attendee) => attendee === username,
+    );
+    if (attendeeIndex === -1) {
+        throw new HTTPError("Already un-RSVPed user", 409);
+    }
+
+    // Remove the user from attendees list
+    const newAttendees = eventAttendees.filter(
+        (attendee) => attendee !== username,
+    );
+
+    return await eventsRepository.updateEvent(id, { attendees: newAttendees });
+}
+
 export default {
     createEvent,
     getEvents,
     getEvent,
     deleteEvent,
+    updateEvent,
+    subscribeEvent,
+    unsubscribeEvent,
 };
