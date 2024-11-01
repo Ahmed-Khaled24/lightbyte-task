5b808b2471c44371aac97a39e7bb3b3c397af2ee
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-11-01 09:17:31 +0200

    feat(controllers): finish up events controller
    the main functionality of the events controller
    has been completely implemented

--- src/controllers/events.controller.ts
+++ src/controllers/events.controller.ts
@@ -1,18 +1,36 @@
 import { Request, Response } from "express";
 import eventsService from "../services/events.service";
 import { Pagination } from "../schemas/pagination.schema";
-import { Event } from "../schemas/events.schema";
+import { CreateEvent, Event, UpdateEvent } from "../schemas/events.schema";
+import { HTTPError } from "../utils/HTTPError";
 
-async function createEvent(req: Request<never, never, Event>, res: Response) {
+async function createEvent(
+    req: Request<never, never, CreateEvent>,
+    res: Response,
+) {
     try {
-        const createdEvent = await eventsService.createEvent(req.body);
-        res.status(201).send({
+        // Combine user data with the event data
+        const completeEvent: Omit<Event, "id" | "attendees"> = {
+            ...req.body,
+            createdBy: req.user.username,
+        };
+        const createdEvent = await eventsService.createEvent(completeEvent);
+
+        res.status(201).json({
             data: createdEvent,
         });
         return;
     } catch (error) {
-        res.status(500).send({
-            data: `Error creating event: ${(error as Error).message}`,
+        let message = `Error creating event: ${(error as Error).message}`;
+        let code = 500;
+
+        if (error instanceof HTTPError) {
+            message = error.message;
+            code = error.status;
+        }
+
+        res.status(500).json({
+            data: message,
         });
     }
 }
@@ -21,12 +39,13 @@ async function getEvents(req: Request<Pagination>, res: Response) {
     try {
         const { page, limit } = req.params;
         const events = await eventsService.getEvents(limit, page);
-        res.status(200).send({
+
+        res.status(200).json({
             data: events,
         });
         return;
     } catch (error) {
-        res.status(500).send({
+        res.status(500).json({
             data: `Error getting events: ${(error as Error).message}`,
         });
     }
@@ -39,24 +58,113 @@ async function getEvent(
     try {
         const { id } = req.params;
         const event = await eventsService.getEvent(id);
-        res.status(200).send({
+
+        res.status(200).json({
             data: event,
         });
         return;
     } catch (error) {
-        res.status(500).send({
+        res.status(500).json({
             data: `Error getting event: ${(error as Error).message}`,
         });
     }
 }
 
-async function deleteEvent(req: Request, res: Response) {}
+async function deleteEvent(
+    req: Request<Required<Pick<Event, "id">>>,
+    res: Response,
+) {
+    try {
+        const { id } = req.params;
+        const deletedEvent = await eventsService.deleteEvent(id);
 
-async function updateEvent(req: Request, res: Response) {}
+        res.status(200).json({
+            data: deletedEvent,
+        });
+        return;
+    } catch (error) {
+        res.status(500).json({
+            data: `Error deleting event: ${(error as Error).message}`,
+        });
+    }
+}
 
-async function subscribeEvent(req: Request, res: Response) {}
+async function updateEvent(
+    req: Request<Required<Pick<Event, "id">>, never, UpdateEvent>,
+    res: Response,
+) {
+    try {
+        const { id } = req.params;
+        const updatedEvent = await eventsService.updateEvent(id, req.body);
 
-async function unsubscribeEvent(req: Request, res: Response) {}
+        res.status(200).json({
+            data: updatedEvent,
+        });
+        return;
+    } catch (error) {
+        res.status(500).json({
+            data: `Error updating event: ${(error as Error).message}`,
+        });
+    }
+}
+
+// RSVP
+async function subscribeEvent(
+    req: Request<Required<Pick<Event, "id">>>,
+    res: Response,
+) {
+    try {
+        const { id } = req.params;
+        const username = req.user.username;
+        await eventsService.subscribeEvent(id, username);
+
+        res.status(200).json({
+            data: "Subscribed to event.",
+        });
+        return;
+    } catch (error) {
+        let message = `Error subscribing: ${(error as Error).message}`;
+        let code = 500;
+
+        if (error instanceof HTTPError) {
+            message = error.message;
+            code = error.status;
+        }
+
+        res.status(500).json({
+            data: message,
+        });
+    }
+}
+
+// un-RSVP
+async function unsubscribeEvent(
+    req: Request<Required<Pick<Event, "id">>>,
+    res: Response,
+) {
+    try {
+        const { id } = req.params;
+        const username = req.user.username;
+        await eventsService.unsubscribeEvent(id, username);
+
+        res.status(200).json({
+            data: "Unsubscribed from event.",
+        });
+        return;
+    } catch (error) {
+        let message = `Error unsubscribing event: ${(error as Error).message}`;
+        let code = 500;
+
+        if (error instanceof HTTPError) {
+            message = error.message;
+            code = error.status;
+        }
+
+        res.status(500).json({
+            data: message,
+        });
+    }
+}
 
 export default {
     createEvent,
